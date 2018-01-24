59e6685dbaedd8315d62d1893c70b6f46666a7c2
Erik Selstam <eselstam@gmail.com>
2018-01-24 16:44:49 +0100

    Fixed indentation

--- src/MatchThree.java
+++ src/MatchThree.java
@@ -1,7 +1,7 @@
 public class MatchThree
 {
-    public static void main(String[] args)
-    {
-	System.out.println("Spooky scary sceleton.");
-    }
+	public static void main(String[] args)
+	{
+		System.out.println("Spooky scary sceleton.");
+	}
 }
