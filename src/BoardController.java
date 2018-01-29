c76d2e338717b861ad6aba9828bf3241e8312a82
Erik Selstam <eselstam@gmail.com>
2018-01-29 03:25:01 +0100

    Detached `Coordinate` class from `BoardController`

--- src/BoardController.java
+++ src/BoardController.java
@@ -7,27 +7,6 @@ import javax.swing.*;
  */
 class BoardController
 {
-	/**
-	 * Stores a two-dimensional coordinate.
-	 */
-	class Coordinate
-	{
-		public int x = 0;
-		public int y = 0;
-		
-		/**
-		 * Constructor for `Coordinate`.
-		 *
-		 * @param x X-coordinate.
-		 * @param y Y-coordinate.
-		 */
-		public Coordinate(int x, int y)
-		{
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
 	private BoardModel model;
 	private BoardView  view;
 	
---
+++ src/Coordinate.java
@@ -0,0 +1,20 @@
+/**
+ * Stores a two-dimensional coordinate.
+ */
+class Coordinate
+{
+	public int x = 0;
+	public int y = 0;
+	
+	/**
+	 * Constructor for `Coordinate`.
+	 *
+	 * @param x X-coordinate.
+	 * @param y Y-coordinate.
+	 */
+	public Coordinate(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
