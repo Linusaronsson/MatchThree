864109b301d8ca053586b20a3b8640bf568e6700
sidju <david.m.o@telia.com>
2018-02-07 15:45:24 +0100

    Added SQL drivers and basic connection to server. Also added Score class, for single scores.

--- src/HighScore.java
+++ src/HighScore.java
@@ -1,11 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+    
 public class HighScore
 {
+    static String url = "jdbc:mysql://sidju.noip.me:3306/java";
+    static String username = "java";
+    static String password = "testeste";
+    
     private int currentScore;
     private String name;
     
     HighScore()
     {
 	currentScore = 0;
+
+	System.out.println("Loading driver...");
+
+	try {
+	    Class.forName("com.mysql.jdbc.Driver");
+	    System.out.println("Driver loaded!");
+	} catch (ClassNotFoundException e) {
+	    throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+	}
+	
+	System.out.println("Connecting database...");
+	try (Connection connection = DriverManager.getConnection(url, username, password)) {
+	    System.out.println("Database connected!");
+	} catch (SQLException e) {
+	    throw new IllegalStateException("Cannot connect the database!", e);
+	}
+	
     }
 
     //Called when game is over
@@ -15,9 +42,9 @@ public class HighScore
     }
 
     //Used to get current highscores
-    public List<int,String> getScoreTable();
+    public List getScoreTable()
     {
-	List<int,String> scoreTable = 0;
+	List scoreTable = new ArrayList<Score>();
 	return scoreTable;
     }
 
@@ -28,13 +55,13 @@ public class HighScore
     }
     
     //called after every move or after the game, to set the score
-    public void addToScore(int points);
+    public void addToScore(int points)
     {
 	currentScore = currentScore + points;
     }
 
     //to get current score, if not stored elsewhere
-    public int getCurrentScore();
+    public int getCurrentScore()
     {
 	return currentScore;
     }
@@ -46,7 +73,8 @@ public class HighScore
     }
 
     //to get the name, if it is to be shown while playing
-    public String getName();
+    public String getName()
     {
 	return name;
     }
+}
---
+++ src/Score.java
@@ -0,0 +1,5 @@
+public class Score
+{
+    public String name;
+    public int score;
+}
