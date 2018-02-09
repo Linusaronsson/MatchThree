6071f1a5e6542033e398d08198577c2a9cd025cb
sidju <david.m.o@telia.com>
2018-02-09 11:44:00 +0100

    Sync created. Class finished.

--- src/HighScore.java
+++ src/HighScore.java
@@ -13,7 +13,9 @@ public class HighScore
     static String username = "java";
     static String password = "testeste";
     static String getScoresQuery = "SELECT * FROM scores ORDER BY scores.score DESC;";
-
+    static String syncQueryStart = "INSERT INTO scores VALUES(";
+    static String syncQueryEnd = ");";
+    
     private Connection connection;
     private int currentScore;
     private String name;
@@ -31,13 +33,39 @@ public class HighScore
 	    throw new IllegalStateException("Cannot find the driver in the classpath!", e);
 	}
 
-	getScoreTable();
     }
 
     //Called when game is over
     public void syncScore()
     {
+	Statement statement;
+
+	// connect database
+	try {
+	    connection = DriverManager.getConnection(url, username, password);
+	    System.out.println("Database connected!");
+	} catch (SQLException e) {
+	    throw new IllegalStateException("Cannot connect the database!", e);
+	}
 	
+	// query database
+	try {statement = connection.createStatement();
+	    statement.executeUpdate(syncQueryStart + currentScore + ", " + "\"" + name + "\"" + syncQueryEnd);
+	    System.out.println("Database responded!");
+	} catch (SQLException e) {
+	    throw new IllegalStateException("Database unresponsive!", e);
+	}
+
+	// disconnect database
+	if (connection != null) {
+	    try {
+		connection.close();
+		connection = null;
+		System.out.println("Database disconnected!");
+	    } catch (SQLException e) {
+		e.printStackTrace();
+	    }
+	}
     }
 
     //Used to get current highscores
@@ -45,10 +73,9 @@ public class HighScore
     {
 	Statement statement;
 	ResultSet selection;
-	
-	Score tmp = new Score();
-	List<Score> scoreTable = new ArrayList<Score>();
+	Score tmp;
 
+	List<Score> scoreTable = new ArrayList<Score>();
 
 	// connect database
 	try {
@@ -69,8 +96,9 @@ public class HighScore
 	// read response
 	try {
 	    selection.first();
-	    while (!selection.isLast())
+	    while ( !selection.isAfterLast() )
 		{
+		    tmp = new Score();
 		    tmp.score = selection.getInt("score");
 		    tmp.name = selection.getString("name");
 		    scoreTable.add(tmp);
@@ -86,6 +114,7 @@ public class HighScore
 	    try {
 		connection.close();
 		connection = null;
+		System.out.println("Database disconnected!");
 	    } catch (SQLException e) {
 		e.printStackTrace();
 	    }
