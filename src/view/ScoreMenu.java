8e272834f654e03e660d274f19b85954b5091592
sidju <david.m.o@telia.com>
2018-02-14 16:07:32 +0100

    Score class replaced by string.

--- src/view/ScoreMenu.java
+++ src/view/ScoreMenu.java
@@ -1,8 +1,35 @@
 package view;
 
-public class ScoreTable
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+import model.HighScore;
+
+
+@SuppressWarnings("serial")
+public class ScoreMenu
+    extends JPanel
 {
-    ScoreTable()
+    //private String base = "Score:            Name:";
+
+    final JList scoreTable = new JList();
+    final JButton refresh = new JButton("Refresh");
+    
+    public ScoreMenu()
     {
+	add(scoreTable);
+	add(refresh);
+    }
+
+    public void refresh()
+    {
+	scoreTable.setModel(new HighScore.getScoreTable());
+    }
+
+    public void addRefreshListener(final ActionListener listener)
+    {
+	refresh.addActionListener(listener);
     }
 }
