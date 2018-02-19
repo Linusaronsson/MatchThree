93667dff1162a0e95399eb5b15d8e952fc75b6b1
Erik Selstam <eselstam@gmail.com>
2018-02-19 19:44:11 +0100

    Sorted imports

--- src/view/MainMenuView.java
+++ src/view/MainMenuView.java
@@ -3,16 +3,14 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
-
+import java.awt.Font;
+import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import util.AssetManager;
 import util.Properties;
 
--- src/view/Window.java
+++ src/view/Window.java
@@ -3,12 +3,10 @@ package view;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.Toolkit;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-
 import util.AssetManager;
 
 /**
