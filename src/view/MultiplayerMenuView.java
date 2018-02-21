da532c0b3499b58a9df91484d67a686f7e601ef1
Erik Tran <erik13792@gmail.com>
2018-02-21 14:13:25 +0100

    Connect button image, border

--- src/view/MultiplayerMenuView.java
+++ src/view/MultiplayerMenuView.java
@@ -1,11 +1,22 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import util.AssetManager;
 
 /**
  * Multiplayer setup screen.
@@ -15,13 +26,44 @@ public class MultiplayerMenuView
 	extends JPanel
 {
 	/** ... */
-	private JButton connect = new JButton("Connect to player");
+	private Button connect = new Button(new ImageIcon(AssetManager.loadImage("ConnectButtonBackground.png")));//"Connect to player");
 	
 	/** ... */
 	private JTextField ip = new JTextField();
 	
 	/** ... */
 	private JTextField port = new JTextField();
+	
+	/**
+	 * 
+	 * 
+	 * ...
+	 */
+	private final class HoverListener 
+	implements MouseListener {
+		
+		/** ... */
+		private Button target = null;
+
+		public HoverListener(final Button target) {
+			this.target = target;
+		}
+
+		@Override public void mouseClicked(final MouseEvent e) { }
+
+		@Override public void mousePressed(final MouseEvent e) { }
+
+		@Override public void mouseReleased(final MouseEvent e) { }
+
+		@Override public void mouseEntered(final MouseEvent e) {
+			target.setMask(Color.BLACK, 0.3f);
+		}
+
+		@Override public void mouseExited(final MouseEvent e) {
+			target.setMask(Color.BLACK, 0.0f);
+		}
+		
+	}
 	
 	/**
 	 * ...
@@ -35,6 +77,20 @@ public class MultiplayerMenuView
 		// Assemble view //
 		// TODO: Add a back button here somehow
 		//add(back);
+		
+		// Set connect button layout //
+		connect.setPreferredSize(new Dimension(160, 80));
+		connect.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createCompoundBorder(
+						BorderFactory.createLineBorder(Color.BLACK, 1), 
+						BorderFactory.createLineBorder(Color.DARK_GRAY, 1)), 
+				BorderFactory.createLineBorder(Color.BLACK, 1)));
+		connect.setBorderPainted(true);
+		JLabel connectLabel = new JLabel("    Connect to player");
+		connectLabel.setFont(new Font("", Font.PLAIN, 15));
+		connect.add(connectLabel);
+		connect.addMouseListener(new HoverListener(connect));
+		
 		add(connect);
 		add(new JLabel("IP: "));
 		add(ip);
